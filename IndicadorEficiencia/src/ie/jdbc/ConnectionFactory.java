9654c0876c04f6427570251978a26008b1a23443
Marcos-Alsleben <mrs_albn@hotmail.com>
2025-07-02 22:41:02 -0300

    Criado projeto, montado estrutura e main

---
+++ IndicadorEficiencia/src/ie/jdbc/ConnectionFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ie.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mrs_a
+ */
+public class ConnectionFactory {
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/indicadoreficiencia?allowPublicKeyRetrieval=true&useSSL=false", "carton", "2574");
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar com banco de dados: " + erro.getMessage());
+            System.exit(1); // Encerra o programa
+            return null; // Este retorno nunca será alcançado, mas é necessário para compilar sem erros.
+        }
+    }
+
+}
---
+++ IndicadorEficiencia/src/ie/jdbc/TestConnection.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ie.jdbc;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mrs_a
+ */
+public class TestConnection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        try {
+
+            new ConnectionFactory().getConnection();
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso ao Banco de Dados flexo!");
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Não foi possível se conectar ao Banco de Dados flexo: " + erro);
+        }
+
+    }
+
+}
